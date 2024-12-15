eec24e0065e305406a54b54e84f18b9720aa22b1
Saad Salman <saadsalmanakram1@gmail.com>
2024-12-15 16:26:23 +0500

    Update Nav.tsx

--- Cloning Websites/nike/src/components/Nav.tsx
+++ Cloning Websites/nike/src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import { headerLogo } from '../assets/images';
 import { hamburger } from '../assets/icons';
-import { navlinks } from '..constants';
+import { navLinks } from '../constants';
 
 const Nav = () => {
   return (
@@ -14,7 +14,25 @@ const Nav = () => {
          />
        </a>
        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+         {navLinks.map((item) => (
+          <li key={item.label}>
+            <a
+              href={item.href}
+              className="font-montserrat leading-normal text-lg text-slate-gray"
+            >
+              {item.label}
+            </a>
+          </li>
+         ))}
        </ul>
+       <div className="hidden max-lg:block">
+         <img 
+           src={hamburger}
+           alt="Hamburger"
+           width={25}
+           height={25}
+         />
+       </div>
       </nav>
     </header>
   )
