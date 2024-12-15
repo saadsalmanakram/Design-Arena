c0eafe126f7f32c155b352e8501d624b8b87022d
Saad Salman <saadsalmanakram1@gmail.com>
2024-12-16 00:52:24 +0500

    Update Button.tsx

--- Cloning Websites/nike/src/components/Button.tsx
+++ Cloning Websites/nike/src/components/Button.tsx
@@ -1,10 +1,13 @@
 const Button = ({ label, iconURL }) => {
 	return (
-		<button className="">
+		<button className="flex justify-center 
+		items-center gap-2 px-7 py-4 border 
+		font-montserrat">
 			{label}
 
 			<img 
 			  src={iconURL} alt="arrow right icon"
+			  className="ml-2 rounded-full w-5 h-5"
 
 			/>
 		</button>
