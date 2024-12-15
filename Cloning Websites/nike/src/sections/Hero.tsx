9638aac43c53b1068ec60651665a222c5dc3e580
Saad Salman <saadsalmanakram1@gmail.com>
2024-12-15 17:23:02 +0500

    Update Hero.tsx

--- Cloning Websites/nike/src/sections/Hero.tsx
+++ Cloning Websites/nike/src/sections/Hero.tsx
@@ -1,8 +1,11 @@
 const Hero = () => {
   return (
-    <div>
-      Hero
-    </div>
+    <section
+      id = "home"
+      className="w-full flex border-2 border-red-500 p-2"
+    >
+
+    </section>
   )
 }
 
