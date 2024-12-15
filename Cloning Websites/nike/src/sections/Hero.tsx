1367756c8b6699bd6da305f42920587d0225d69a
Saad Salman <saadsalmanakram1@gmail.com>
2024-12-15 20:08:22 +0500

    Update Hero.tsx

--- Cloning Websites/nike/src/sections/Hero.tsx
+++ Cloning Websites/nike/src/sections/Hero.tsx
@@ -2,8 +2,14 @@ const Hero = () => {
   return (
     <section
       id = "home"
-      className="w-full flex border-2 border-red-500 p-2"
+      className="w-full flex xl:flex-row flex-col 
+      justify-center min-h-screen gap-10 max-container"
     >
+      <div className="relative xl:w-2/5 flex flex-col
+      justify-center items-start w-full max-xl:padding-x
+      pt-28">
+        
+      </div>
 
     </section>
   )
