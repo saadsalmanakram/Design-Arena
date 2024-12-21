d5e7f622d34ca67250e7a24d696a05e92a66a227
Saad Salman <saadsalmanakram1@gmail.com>
2024-12-21 21:01:37 +0500

    update

--- Time Pass Projects/Movies Listings/Movies List/src/App.tsx
+++ Time Pass Projects/Movies Listings/Movies List/src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="bg-sky-700 hover:bg-sky-700">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
